d5ec389bfe94c88f4f0a71557293d2b12ce7a0b3
beck <b.tadesse.77@gmail.com>
2022-11-19 00:17:40 +0300

    feat: add edge function

---
+++ api/typescript/edge.ts
@@ -0,0 +1,43 @@
+import { geolocation, ipAddress } from '@vercel/edge';
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+export default function handler(request: Request) {
+  try {
+    const parts = request.url.split('?');
+    const params = parts ? parts[1] : undefined;
+    const name =
+      params
+        ?.split('&')
+        .find((param) => param.startsWith('name'))
+        ?.split('=')[1] ?? 'Dave';
+
+    return new Response(
+      JSON.stringify({
+        message: `Hello ${name} from the edge.`,
+        // url: request.url,
+        server: {
+          ipAddress: ipAddress(request),
+          geolocation: geolocation(request),
+        },
+      }),
+      {
+        status: 200,
+        statusText: 'Success',
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        error: {
+          message: 'Internal Server error',
+        },
+      }),
+      {
+        status: 502,
+      }
+    );
+  }
+}
