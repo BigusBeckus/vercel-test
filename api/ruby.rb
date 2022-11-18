eb7a2da3ac95b0bd2b752578c61506f4d85a2cae
beck <b.tadesse.77@gmail.com>
2022-11-18 23:15:03 +0300

    chore: remove ruby function

--- api/ruby.rb
+++
@@ -1,7 +0,0 @@
-# Full disclosure: I've never even touched Ruby before :(
-Handler = Proc.new do [req, res]
-  res.status = 200
-  res['Content-Type'] = 'text/text; charset=utf-8'
-  # res.body = "Hello #{} from the Ruby file."
-  res.body = "#{req}"
-end
